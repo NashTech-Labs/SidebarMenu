21631ddb675195e2b45cad289926fe9b6204d1c1
Anirudh17072000 <anirudh.chauhan@knoldus.com>
2022-03-28 19:26:53 +0530

    sidebar navmenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'theRichPost';
+  status: boolean = false;
+  clickEvent(){
+      this.status = !this.status;       
+  }
 }
